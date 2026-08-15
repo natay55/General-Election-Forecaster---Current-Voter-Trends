cfad8b6d61a9386fef01269d33183f2e7580c17c
IdotEXE-313 <mcpegamerxx1@gmail.com>
2026-08-15 21:10:24 +0100

    added server starting on frontend

--- frontend/app.py
+++ frontend/app.py
@@ -245,5 +245,7 @@ app.layout = html.Div(
 #--------------------------------------------------
 # Run
 #--------------------------------------------------
+server = app.server
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=False, host="0.0.0.0", port=port)
